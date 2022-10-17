ef21df310b3d7e8902c6bfcd2621f09fd6713dc9
matthewcroy <mcro427@aucklanduni.ac.nz>
2022-10-17 13:17:24 +1300

    cookies implemented
    cookies

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,10 @@ if (localStorage.getItem(‘cookieSeen’) != ‘shown’) {
   $(‘.close’).click(function() {
     $(‘.cookie-banner’).fadeOut();
   })
+fetch('https://api.ipregistry.co/?key=tryout')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (payload) {
+        console.log(payload.location.country.name + ', ' + payload.location.city);
+    });
